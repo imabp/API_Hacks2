96702ea35248cf62bc3c1bb569efcd750a6e1dd4
ankitzm <ankit.singh.zm@gmail.com>
2021-07-02 07:55:50 +0530

    unnessasary file removed

--- app/src/Components/Socials/Socials.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { SocialIcon } from "react-social-icons";
-import "./Socials.css";
-
-function Socials() {
-  return <div className="socials"></div>;
-}
-
-export default Socials;
