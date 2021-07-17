7690db6b609d41915eb700e0a58f8688b64f2066
Ashish Chawda <56932286+pixan198@users.noreply.github.com>
2021-07-17 17:00:55 +0000

    remove unused image and add alt text

--- app/src/Components/ImageComponent/ImageComponent.js
+++ app/src/Components/ImageComponent/ImageComponent.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./ImageComponent.css";
-import UFO from "../../assets/ufo.gif";
+//import UFO from "../../assets/ufo.gif";
 import Icon from './image.svg'
 function ImageComponent() {
   return (
     <div className="image-component">
-      <img src={Icon} width="90%"/>
+      <img alt="" src={Icon} width="90%"/>
     </div>
   );
 }
