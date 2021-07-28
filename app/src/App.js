74c1dd2f86887cfda219bc1c440c92c9f710487d
ankitzm <ankit.singh.zm@gmail.com>
2021-07-28 19:39:55 +0530

    new layout

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import ImageComponent from './Components/ImageComponent/ImageComponent';
-import LandingComponent from './Components/LandingComponent/LandingComponent';
 
 function App() {
   return (
     <div className="App">
-      <ImageComponent />
-      <LandingComponent />
+      sdee
     </div>
   );
 }
