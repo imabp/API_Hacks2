2f2098f7ef3cdc2954d28627630445814d04be26
ankitzm <ankit.singh.zm@gmail.com>
2021-07-29 19:57:49 +0530

    sidebar completed

--- app/src/App.js
+++ app/src/App.js
@@ -9,6 +9,7 @@ import {
   Link
 } from "react-router-dom";
 
+import Home from "./Components/Home"
 import Sponser from './Components/Sponser';
 import Workshop from "./Components/Workshop";
 import Theme from './Components/Theme'
@@ -17,37 +18,47 @@ function App() {
   return (
 
     <Router>
-      <div  className="flex flex-row">
+      <div  className="flex flex-row relative">
 
       {/* A <Switch> looks through its children <Route>s and
     renders the first one that matches the current URL. */}
 
       <Switch>
-        <Route path="/sponsers">
-          <Sponser />
-        </Route>
-        <Route path="/theme">
-          <Theme />
-        </Route>
-        <Route path="/workshop">
-          <Workshop />
-        </Route>
+      <Route exact path="/" component={Home} />
+        <Route path="/sponsers" component={Sponser} />
+        <Route path="/theme" component={Theme} />
+        <Route path="/workshop" component={Workshop} />
       </Switch>
 
 
-        <nav className="w-1/3 bg-gray-500 h-screen justify">
-          <ul>
+        <div className="w-1/3 h-screen absolute right-0 flex nav-background">
+          <ul className="m-auto w-full flex flex-col justify-center text-center">
             <li>
-              <Link to="/sponsers">sponsers</Link>
+            What you Looking for?
+            <br></br>
+            Might be here ;)
             </li>
-            <li>
-              <Link to="/theme">theme</Link>
+
+            <li className="nav-link"> 
+              <Link to="/what-is-api">What is API hacks ?</Link>
             </li>
-            <li>
-              <Link to="/workshop">Workshop</Link>
+            <li className="nav-link">
+              <Link to="/theme">Themes</Link>
+            </li>
+            <li className="nav-link"> 
+              <Link to="/workshop">Awesome Workshops</Link>
+            </li>
+            <li className="nav-link"> 
+              <Link to="/sponsers">Sponsers</Link>
+            </li>
+            <li className="nav-link">
+              <Link to="/blog">Our Hacker Blog</Link>
+            </li>
+            <li className="nav-link">
+              <Link to="/hiring">Network and Hiring</Link>
             </li>
           </ul>
-        </nav>
+        </div>
 
       </div>
     </Router>
