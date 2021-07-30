1b9d1432113e4e791feeb59c5bfcf24b1b7d4d04
ankitzm <ankit.singh.zm@gmail.com>
2021-07-30 20:34:13 +0530

    responsiveness added

--- app/src/Components/Home.js
+++ app/src/Components/Home.js
@@ -4,24 +4,25 @@ import "./Home.css"
 function Home() {
     return (
         <div className="home text-xl pl-12 pt-10 h-screen absolute">
-            <div className="text-6xl">
+            <div className="text-5xl lg:text-6xl">
                 API Hacks<br></br>
                 <span className="font-black">2.0</span>
             </div>
 
-            <div className="text-4xl mt-4">
+            <div className="text-3xl lg:text-4xl mt-4">
                 9th-10th October, 2021
             </div>
 
-            <div className="mt-20 text-3xl">
+            <div className="mt-14 lg:mt-20 text-2xl lg:text-3xl">
                 Build your <br></br>
                 Digital Footprint as <br></br>
                 a Student Developer <br></br>
             </div>
 
-            <button class="text-black mt-4 py-2 px-4 btn">
+            <a href="http://discord.apihacks.co/" >
+            <button class="text-base lg:text-lg text-black mt-4 py-2 px-4 btn">
                 Interested? Join Us
-            </button>
+            </button> </a>
 
             <div class="absolute bottom-0 pb-10">
                 Want to talk to us? <br></br>
