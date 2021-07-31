20f321947d598686fb53f4b00a2502ed158675a0
Abir <abir.pal899@gmail.com>
2021-07-31 18:30:39 +0530

    Add Routes Config

---
+++ app/src/routes.js
@@ -0,0 +1,59 @@
+import { Sponsor, Workshop, Theme } from "./Components";
+
+const ROUTES = [
+  {
+    path: "/what-is-api",
+    displayName: "What is an API?",
+    isExternal: false,
+    exact: true,
+    component: () => <></>,
+  },
+  {
+    path: "/theme",
+    displayName: "Themes",
+    isExternal: false,
+    exact: true,
+    component: () => (
+      <>
+        <Theme />
+      </>
+    ),
+  },
+  {
+    path: "/workshop",
+    displayName: "Awesome Workshops",
+    isExternal: false,
+    exact: true,
+    component: () => (
+      <>
+        <Workshop />
+      </>
+    ),
+  },
+  {
+    path: "/sponsors",
+    displayName: "Sponsors",
+    isExternal: false,
+    exact: true,
+    component: () => (
+      <>
+        <Sponsor />
+      </>
+    ),
+  },
+  {
+    path: "/blog",
+    displayName: "Our Hacker Media",
+    isExternal: false,
+    exact: true,
+    component: () => <></>,
+  },
+  {
+    path: "/hiring",
+    displayName: "Network and Hiring",
+    isExternal: false,
+    exact: true,
+    component: () => <></>,
+  },
+];
+export default ROUTES;
