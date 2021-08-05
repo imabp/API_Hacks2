be5c6ead587f5f50508f6db04c82de7c7fb8286d
ankitzm <ankit.singh.zm@gmail.com>
2021-08-05 23:32:36 +0530

    minor design update

--- app/src/Components/Home.js
+++ app/src/Components/Home.js
@@ -6,7 +6,7 @@ import "./Home.css";
 class Home extends React.Component {
   render() {
     return (
-      <div className="home text-xl pl-12 pt-10 h-screen absolute">
+      <div className="home text-xl pl-6 md:pl-12 pt-10 h-screen absolute">
         <motion.div
           className="circle"
           animate={{
@@ -22,29 +22,32 @@ class Home extends React.Component {
             left: -200,
           }}
         />
-        <div className="text-5xl lg:text-6xl">
+        <div className="text-5xl md:text-6xl">
           <Fade left cascade>
             API Hacks
-            <span className="font-black">2.0</span>{" "}
+            <span className="font-black">2.0</span>
           </Fade>
         </div>
 
-        <div className="text-3xl lg:text-4xl mt-4">
+        <div className="text-2xl md:text-4xl mt-2">
           <Fade left cascade>
             9th-10th October, 2021
           </Fade>
         </div>
 
-        <div className="mt-14 lg:mt-20 text-2xl lg:text-3xl">
+        <div className="mt-12 md:mt-20 text-2xl md:text-3xl">
+
           <Fade left cascade>
-            Build your <br />
-            Digital Footprint as <br />a Student Developer <br />
+            Build your{""}
+            Digital Footprint as{""}
+            a Student Developer
           </Fade>
+
         </div>
 
         <Fade left cascade>
           <a href="http://discord.apihacks.co/">
-            <button class="text-base lg:text-lg text-black mt-4 py-2 px-4 btn">
+            <button class="text-base md:text-lg text-black mt-4 py-2 px-4 btn">
               <Fade left cascade>
                 Interested? Join Us
               </Fade>
@@ -52,10 +55,9 @@ class Home extends React.Component {
           </a>
         </Fade>
 
-        <div class="absolute bottom-0 pb-10">
+        <div class="absolute bottom-0 pb-6">
           <Fade left cascade>
-            {" "}
-            Want to talk to us? <br></br>
+            Want to talk to us? {""}
             <span className="font-bold">staff@apihacks.co</span>
           </Fade>
         </div>
