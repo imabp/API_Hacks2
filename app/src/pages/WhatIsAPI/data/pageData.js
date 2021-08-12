912a17cd93d986492535d6e39ed36213f66e3c29
ankitzm <ankit.singh.zm@gmail.com>
2021-08-12 20:15:39 +0530

    fixed button issue

--- app/src/pages/WhatIsAPI/data/pageData.js
+++ app/src/pages/WhatIsAPI/data/pageData.js
@@ -1,17 +1,17 @@
 const pageData = {
   pageData: [
     {
-      gif: "https://cdn.discordapp.com/attachments/874607271848607754/874608902963085372/request-new.gif",
+      gif: "https://cdn.discordapp.com/attachments/874607271848607754/875381942860775444/ezgif.com-gif-maker.gif",
       text: "Carl asked his driver to help him reach carl santos.",
       inference: "(API called)",
     },
     {
-      gif: "https://cdn.discordapp.com/attachments/874607271848607754/874611343444049920/working-new.gif",
+      gif: "https://cdn.discordapp.com/attachments/874607271848607754/875381938490331156/ezgif.com-gif-maker1.gif",
       text: "Carl's driver took him way too fast.",
       inference: "(Carl's Driver = API Working)",
     },
     {
-      gif: "https://cdn.discordapp.com/attachments/874607271848607754/874611305875656724/response-new.gif",
+      gif: "https://media.discordapp.net/attachments/874607271848607754/875381930101706792/ace-ventura-jim-carey.gif",
       text: "Carl finally reached Los Santos within 2 hours.",
       inference: "(Response Received from API)",
     },
