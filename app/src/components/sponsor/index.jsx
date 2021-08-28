a685c27ee3148b381e65fe6800fdcaba779fb1ad
Abir <abir.pal899@gmail.com>
2021-08-29 00:13:03 +0530

    Add FontAwesome

--- app/src/components/sponsor/index.jsx
+++ app/src/components/sponsor/index.jsx
@@ -14,7 +14,7 @@ const SponsorCard = (props) => {
 
                 <div className="px-6 py-4">
                  
-                    <p className={`text-center typebox-${type} sm:text-xs md:text-base`}>
+                    <p className={`text-center typebox-${type} sm:text-xs md:text-base rounded-sm`}>
                         {type} partner   </p>
                 </div>
                 <a href={url} target="_blank" rel="noopenner noreferrer"> <img className="px-4 object-scale-down w-full h-10 " src={img} alt={organization} /></a>
